9a83c48003694893e81f729a9d5773ca9e1a3615
rayantan <rayantan2003@gmail.com>
2025-04-09 19:50:07 +0530

    this files are added in signup and login pages

---
+++ usersignup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>login Form</title>
+  <link rel="stylesheet" href="usersignup.css">
+</head>
+<body>
+  <div class="container">
+  <form class="signup-form" action="signup.php" method="POST">
+     <strong><h2>Signup</h2></strong> 
+      <input type="text" name="username" placeholder="Username" required>
+      <input type="text" name="Address" placeholder="Address" required>
+      <input type="text" name="Gender" placeholder="Gender" required>
+      <input type="text" name="Phone Number" placeholder="Phone Number" required>
+      <input type="email" name="Email" placeholder="Email" required>
+      <input type="password" name="Password" placeholder="Password" required>
+      <input type="password" name="Confirm_password" placeholder="Confirm Password" required>
+      <button type="submit" name="register">Register</button>
+      <p class="login-link">Already have an account? <a href="http://localhost/foodWaste/userLogin.php">Login</a></p>
+    </form>
+
+
+
+  </div>
+</body>
+</html>
