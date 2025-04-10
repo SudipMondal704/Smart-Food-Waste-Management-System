65e86e892b69a3f73d59324a3ffffb0df968ac8d
rayantan <rayantan2003@gmail.com>
2025-04-10 09:39:43 +0530

    Gender selection process changed to radio button

--- usersignup.php
+++ usersignup.php
@@ -12,13 +12,19 @@
      <strong><h2>Signup</h2></strong> 
       <input type="text" name="username" placeholder="Username" required>
       <input type="text" name="Address" placeholder="Address" required>
-      <input type="text" name="Gender" placeholder="Gender" required>
+      <label>Select Gender:</label>
+      <input type="radio" name="gender" value="male">
+      <label for="male">Male</label>
+      <input type="radio" name="gender" value="female">
+      <label for="female">Female</label>
+      <input type="radio" name="gender" value="other">
+      <label for="other">Other</label>
       <input type="text" name="Phone Number" placeholder="Phone Number" required>
       <input type="email" name="Email" placeholder="Email" required>
       <input type="password" name="Password" placeholder="Password" required>
       <input type="password" name="Confirm_password" placeholder="Confirm Password" required>
       <button type="submit" name="register">Register</button>
-      <p class="login-link">Already have an account? <a href="http://localhost/foodWaste/userLogin.php">Login</a></p>
+      <p class="login-link">Already have an account? <a href="http://localhost/foodWaste/Smart-Food-Waste-Management-System/userLogin.php">Login</a></p>
     </form>
 
 
