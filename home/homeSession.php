99e79a315e8496bb43ebe8dad85a7e973d0424ca
Anjan0212 <sahaanjan2004@gmail.com>
2025-06-09 20:54:29 +0530

    REFRESH CODE

--- home/homeSession.php
+++ home/homeSession.php
@@ -71,7 +71,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Food Donate - Save Food Share joy</title>
+    <title>easyDonate - Save Food Share joy</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="../css/style.css" rel="stylesheet">
 </head>
